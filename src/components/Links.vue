6566db8861b72899b6b6ca526b909346674571b1
siddv <me@siddv.net>
2018-12-25 23:09:41 +0000

    feat: update link styles

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,12 +1,13 @@
 <template>
   <section class="section">
-    <section class="container content">
-      <h1>{{title || 'Secret Links'}}</h1>
-      <ul>
-        <li
+    <section class="container">
+      <h1 class="title">{{title || 'Secret Links'}}</h1>
+      <ul class="columns is-multiline">
+        <li class="column is-half-tablet is-one-quarter-desktop"
           v-for="link in links"
           :key="link.href">
           <a
+            class="button is-info is-large is-outlined is-fullwidth"
             :href="link.href"
             target="_blank"
             rel="noopener">
@@ -28,7 +29,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  li {
-    padding-bottom: 1em;
+  .container {
+    max-width: 960px;
   }
 </style>
